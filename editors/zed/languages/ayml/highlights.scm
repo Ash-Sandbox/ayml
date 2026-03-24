6af0aaa84a6539341853f3b469051ca6f4165ecb
Alex Shapiro <alex@alexshapiro.xyz>
2026-03-24 11:44:50 -0400

    move tree-sitter-ayml to its own repo

--- editors/zed/languages/ayml/highlights.scm
+++ editors/zed/languages/ayml/highlights.scm
@@ -1,1 +1,22 @@
-../../../../tree-sitter-ayml/queries/highlights.scm
+; Comments
+(comment) @comment
+
+; Mapping keys (includes the trailing colon)
+(mapping_key) @property
+
+; Literals
+(null_literal) @constant.builtin
+(boolean_literal) @constant.builtin
+(integer_literal) @number
+
+; Strings
+(double_quoted_string) @string
+(triple_quoted_string) @string
+(bare_string) @string
+(flow_bare_string) @string
+
+; Punctuation
+["," ] @punctuation.delimiter
+(sequence_indicator) @punctuation.delimiter
+["[" "]"] @punctuation.bracket
+["{" "}"] @punctuation.bracket
